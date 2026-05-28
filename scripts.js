ff04da1d8757ef89877cdf82b8e8c2989b2037f2
Gabriel Martinelli <gabrielmartinelli22@gmail.com>
2026-05-28 18:32:42 -0300

    dia 4 - interacao com javascript

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-let cliques = localStorage.getItem("cliques") || 0;
-
-cliques = Number(cliques);
-
-document.getElementById("contador").innerText = "Cliques: " + cliques;
+let cliques = 0;
 
 document.addEventListener("click", function() {
   cliques++;
 
-  localStorage.setItem("cliques", cliques);
-
   document.getElementById("contador").innerText = "Cliques: " + cliques;
+
+  // gera cor aleatória
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  let cor = "rgb(" + r + "," + g + "," + b + ")";
+
+  document.body.style.background = cor;
 });
