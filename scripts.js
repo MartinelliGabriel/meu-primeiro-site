a11895d958c12346d7b7940769e48bfbd3d266aa
Gabriel Martinelli <gabrielmartinelli22@gmail.com>
2026-05-28 17:50:10 -0300

    dia 4 - fundo muda com cliques

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
 let cliques = 0;
 
 function clicarBotao() {
-  cliques = cliques + 1;
+  cliques++;
 
   document.getElementById("contador").innerText = "Cliques: " + cliques;
 
   if (cliques < 5) {
     document.getElementById("texto").innerText = "Tá começando 😄";
-  } else if (cliques < 10) {
-    document.getElementById("texto").innerText = "Já tá pegando o jeito 😎";
-  } else {
-    document.getElementById("texto").innerText = "VICIADO EM CLICAR 😂🔥";
+    document.body.style.background = "#0d0d0d";
+  } 
+  else if (cliques < 10) {
+    document.getElementById("texto").innerText = "Tá evoluindo 😎";
+    document.body.style.background = "#003322";
+  } 
+  else {
+    document.getElementById("texto").innerText = "NÍVEL MÁXIMO 🔥";
+    document.body.style.background = "#00ff88";
   }
 }
