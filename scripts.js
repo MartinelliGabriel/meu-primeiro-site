7d8aab7e9c1c45265e8decd2dfa31c646f2c36ed
Gabriel Martinelli <gabrielmartinelli22@gmail.com>
2026-05-28 18:03:39 -0300

    dia 4 - fundo muda com cliques suavemente

--- scripts.js
+++ scripts.js
@@ -1,20 +1,16 @@
 let cliques = 0;
 
-function clicarBotao() {
+document.addEventListener("click", function() {
   cliques++;
 
   document.getElementById("contador").innerText = "Cliques: " + cliques;
 
-  if (cliques < 5) {
-    document.getElementById("texto").innerText = "Tá começando 😄";
-    document.body.style.background = "#0d0d0d";
-  } 
-  else if (cliques < 10) {
-    document.getElementById("texto").innerText = "Tá evoluindo 😎";
-    document.body.style.background = "#003322";
-  } 
-  else {
-    document.getElementById("texto").innerText = "NÍVEL MÁXIMO 🔥";
-    document.body.style.background = "#00ff88";
-  }
-}
+  // gera cor aleatória
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  let cor = "rgb(" + r + "," + g + "," + b + ")";
+
+  document.body.style.background = cor;
+});
