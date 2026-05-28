4f9ea2c5d9384b2f0363fe8871702baeda284541
Gabriel Martinelli <gabrielmartinelli22@gmail.com>
2026-05-28 18:11:45 -0300

    dia 4 - interacao com js e cores dinamicas

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
-let cliques = 0;
+let cliques = localStorage.getItem("cliques") || 0;
+
+cliques = Number(cliques);
+
+document.getElementById("contador").innerText = "Cliques: " + cliques;
 
 document.addEventListener("click", function() {
   cliques++;
 
+  localStorage.setItem("cliques", cliques);
+
   document.getElementById("contador").innerText = "Cliques: " + cliques;
-
-  // gera cor aleatória
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-
-  let cor = "rgb(" + r + "," + g + "," + b + ")";
-
-  document.body.style.background = cor;
 });
