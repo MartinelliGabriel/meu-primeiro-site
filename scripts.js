1265db15ccb53799ca6c46ab30626260b50230e5
Gabriel Martinelli <gabrielmartinelli22@gmail.com>
2026-05-28 17:40:06 -0300

    dia 4 - adicionando logica com if

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+let cliques = 0;
+
 function clicarBotao() {
-  document.getElementById("texto").innerText = "Agora sou dev de verdade 😎";
+  cliques = cliques + 1;
+
+  document.getElementById("contador").innerText = "Cliques: " + cliques;
+
+  if (cliques < 5) {
+    document.getElementById("texto").innerText = "Tá começando 😄";
+  } else if (cliques < 10) {
+    document.getElementById("texto").innerText = "Já tá pegando o jeito 😎";
+  } else {
+    document.getElementById("texto").innerText = "VICIADO EM CLICAR 😂🔥";
+  }
 }
